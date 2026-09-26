8ecc8456e90f981008529955818c36e41eaa60c3
Brian Lovely <brian.lovely.23@gmail.com>
2026-09-26 09:17:00 -0400

    Fix overlapping debris drop ranges

--- debrisHandler.php
+++ debrisHandler.php
@@ -34,63 +34,44 @@ public function howManyPieces(){
 }
 
 public function getRocketDebris(){
-    $type;
     $roll = $this->rollDie(100);
-    if($roll > 0 && $roll <= 5){
+    if($roll <= 5){
         $type = new Cone();
-    }
-    if($roll > 5 && $roll <= 10){
+    } elseif($roll <= 10){
         $type = new gSystem();
-    }
-    if($roll > 1 && $roll <= 15){
+    } elseif($roll <= 15){
         $type = new Fins();
-    }
-    if($roll > 15 && $roll <= 25){
+    } elseif($roll <= 25){
         $type = new Tube();
-    }
-    if($roll > 25 && $roll <= 30){
+    } elseif($roll <= 30){
         $type = new Explosive();
-    }
-    if($roll > 30 && $roll <= 35){
+    } elseif($roll <= 35){
         $type = new hShield();
-    }
-    if($roll > 35 && $roll <= 40){
+    } elseif($roll <= 40){
         $type = new Nozzle();
-    }
-    if($roll > 40 && $roll <= 45){
+    } elseif($roll <= 45){
         $type = new fTank();
-    }
-    if($roll > 45 && $roll <= 50){
+    } elseif($roll <= 50){
         $type = new Phalanges();
-    }
-    if($roll > 50 && $roll <= 55){
+    } elseif($roll <= 55){
         $type = new bMat();
-    }
-    if($roll > 55 && $roll <= 60){
+    } elseif($roll <= 60){
         $type = new Needle();
-    }
-    if($roll > 60 && $roll <= 65){
+    } elseif($roll <= 65){
         $type = new Latch();
-    }
-    if($roll > 65 && $roll <= 70){
+    } elseif($roll <= 70){
         $type = new Highlighter();
-    }
-    if($roll > 70 && $roll <= 75){
+    } elseif($roll <= 75){
         $type = new Itinerary();
-    }
-    if($roll > 75 && $roll <= 80){
+    } elseif($roll <= 80){
         $type = new Phrasebook();
-    }
-    if($roll > 80 && $roll <= 85){
+    } elseif($roll <= 85){
         $type = new Lightning();
-    }
-    if($roll > 85 && $roll <= 90){
+    } elseif($roll <= 90){
         $type = new Chain();
-    }
-    if($roll > 90 && $roll <= 95){
+    } elseif($roll <= 95){
         $type = new jEdge();
-    }
-    if($roll > 95 && $roll <= 100){
+    } else {
         $type = new Label();
     }
 
